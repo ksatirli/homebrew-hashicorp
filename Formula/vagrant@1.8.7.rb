6531590dbcb32aac63f7d3021e70d6bf86c9b92a
Kerim Satirli <k.satirli@synapticate.com>
2017-07-12 13:57:52 +0200

    fixes SHA256

--- Formula/vagrant@1.8.7.rb
+++ Formula/vagrant@1.8.7.rb
@@ -1,9 +1,9 @@
 class VagrantAT187 < Formula
   desc "Vagrant 1.8.7"
   version "1.8.7"
-  homepage 'https://www.vagrantup.com/'
+  homepage "https://www.vagrantup.com/"
   url "https://releases.hashicorp.com/vagrant/#{version}/vagrant_#{version}.dmg"
-  sha256 'ac16217b114d8816264700e41114b62acd0c071d2699ae13954086ae3fcb151c'
+  sha256 "14d68f599a620cf421838ed037f0a1c4467e1b67475deeff62330a21fda4937b"
 
   # depends_on "cmake" => :build
 
