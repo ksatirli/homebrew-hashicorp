69b60808e9ec5b8bd8b37c4c891bb9ab13b64274
Kerim Satirli <k.satirli@synapticate.com>
2017-07-12 14:29:25 +0200

    adds 1.9.5

---
+++ Casks/vagrant@1.9.5.rb
@@ -0,0 +1,22 @@
+cask 'vagrant@1.9.5' do
+  version '1.9.5'
+  sha256 'ac16217b114d8816264700e41114b62acd0c071d2699ae13954086ae3fcb151c'
+
+  url "https://releases.hashicorp.com/vagrant/#{version}/vagrant_#{version}_x86_64.dmg"
+  name 'Vagrant'
+  homepage 'https://www.vagrantup.com/'
+
+  pkg "vagrant.pkg", allow_untrusted: true
+
+  uninstall script:  {
+                       executable: 'uninstall.tool',
+                       input:      ['Yes'],
+                       sudo:       true,
+                     },
+            pkgutil: 'com.vagrant.vagrant'
+
+  zap       trash: [
+                      '~/.vagrant.d'
+                    ]
+
+end
