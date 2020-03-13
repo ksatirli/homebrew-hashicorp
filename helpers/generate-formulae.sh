5a2720b7de51e69006309a43cb9fad47cd48b9cf
Kerim Satirli <k.satirli@gmail.com>
2020-03-13 17:16:14 +0100

    linting

--- helpers/generate-formulae.sh
+++ helpers/generate-formulae.sh
@@ -8,17 +8,6 @@ set -e
 # shellcheck source=./helpers/FUNCTIONS.sh
 . "./helpers/FUNCTIONS.sh"
 
-PACKAGE_FLAG=""
-
-#for VERSION in "${CONSUL_1XX[@]}"; do
-#  generate_formula "consul" "${VERSION}" "${CASK_ARCHITECTURE}"
-#done
-
-# generate Nomad formula and set prefix (`./`) for checksums
-#for VERSION in "${NOMAD_PREFIXED[@]}"; do
-#  generate_formula "nomad" "${VERSION}" "${FORMULA_ARCHITECTURE}" "./"
-#done
-
-for VERSION in "${NOMAD_NONPREFIXED[@]}"; do
-  generate_formula "nomad" "${VERSION}" "${FORMULA_ARCHITECTURE}"
+for VERSION in "${CONSUL_1XX[@]}"; do
+  generate_formula "consul" "${VERSION}" "${FORMULA_ARCHITECTURE}"
 done
