71970ce275453a922efeaab3a26bdf4ca5d23bdf
Kerim Satirli <k.satirli@gmail.com>
2020-03-13 14:56:58 +0100

    adds Consul 1.4.x formulae

---
+++ Formula/consul@1.4.4.rb
@@ -0,0 +1,55 @@
+class ConsulAT144 < Formula
+  desc "Tool for service discovery, monitoring and configuration"
+  homepage "https://www.consul.io"
+  url "https://releases.hashicorp.com/consul/1.4.4/consul_1.4.4_darwin_amd64.zip"
+  sha256 "804a513d4482f214e7124bfa57d7d63d55156abb928e743e1cdfa041b7b131a2"
+
+  def install
+    bin.install "consul"
+  end
+
+  plist_options :manual => "consul agent -dev -advertise 127.0.0.1"
+
+  def plist; <<~EOS
+    <?xml version="1.0" encoding="UTF-8"?>
+    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+    <plist version="1.0">
+      <dict>
+        <key>KeepAlive</key>
+        <dict>
+          <key>SuccessfulExit</key>
+          <false/>
+        </dict>
+        <key>Label</key>
+        <string>#{plist_name}</string>
+        <key>ProgramArguments</key>
+        <array>
+          <string>#{opt_bin}/consul</string>
+          <string>agent</string>
+          <string>-dev</string>
+          <string>-advertise</string>
+          <string>127.0.0.1</string>
+        </array>
+        <key>RunAtLoad</key>
+        <true/>
+        <key>WorkingDirectory</key>
+        <string>#{var}</string>
+        <key>StandardErrorPath</key>
+        <string>#{var}/log/consul.log</string>
+        <key>StandardOutPath</key>
+        <string>#{var}/log/consul.log</string>
+      </dict>
+    </plist>
+  EOS
+  end
+
+  test do
+    assert_match "consul version 1.4.4", shell_output("#{bin}/consul -version", 2)
+
+    fork do
+      exec "#{bin}/consul", "agent", "-data-dir", "."
+    end
+    sleep 3
+    system "#{bin}/consul", "leave"
+  end
+end
