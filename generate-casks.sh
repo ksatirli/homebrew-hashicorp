5cad50a72370dd8ebda5ca5b286a8444ff398ac2
Kerim Satirli <k.satirli@gmail.com>
2020-04-09 11:16:23 +0200

    updates logic for Cask generation

--- generate-casks.sh
+++ generate-casks.sh
@@ -2,138 +2,178 @@
 
 set -e
 
+PACKAGE_PROTOCOL="https"
+PACKAGE_HOST="releases.hashicorp.com"
+CHECKSUM_URL_BASE="${PACKAGE_PROTOCOL}://${PACKAGE_HOST}"
+CASK_FILE="templates/generic.cask"
+
 # shellcheck source=./VERSIONS.sh
 . "./VERSIONS.sh"
-PRODUCT_PACKAGE_HOST="https://releases.hashicorp.com"
 
 # shellcheck source=../homebrew-tools/FUNCTIONS.sh
 . "../homebrew-tools/FUNCTIONS.sh"
 
 case "${1}" in
   consul)
-    # generates Casks for Consul
+    BINARY="${1}"
+    NAME="Consul"
+    HOMEPAGE="https:\/\/www.consul.io\/"
+
     for VERSION in "${CONSUL_1XX[@]}"; do
       # shellcheck disable=SC2034
-      PRODUCT_NAME="consul"
-      PRODUCT_VERSION="${VERSION}"
-      PRODUCT_VERSION_CLEAN="${PRODUCT_VERSION}"
-      PRODUCT_PACKAGE_PATH="${PRODUCT_NAME}_${PRODUCT_VERSION}_darwin_amd64.zip"
-      PRODUCT_CHECKSUM_URL="${PRODUCT_PACKAGE_HOST}/${PRODUCT_NAME}/${PRODUCT_VERSION}/${PRODUCT_NAME}_${PRODUCT_VERSION}_SHA256SUMS"
-      PRODUCT_CHECKSUM_PATTERN="${PRODUCT_PACKAGE_PATH}"
-
+      PACKAGE_PATH="${BINARY}_${VERSION}_darwin_amd64.zip"
+      CHECKSUM_URL="${CHECKSUM_URL_BASE}/${BINARY}/${VERSION}/${BINARY}_${VERSION}_SHA256SUMS"
+      CHECKSUM_PATTERN="${PACKAGE_PATH}"
       generate_cask
     done
     ;;
 
   consul-aws)
-    # generates Casks for consul-aws
+    BINARY="${1}"
+    NAME="${1}"
+    HOMEPAGE="https:\/\/github.com\/hashicorp\/consul-aws"
+
     for VERSION in "${CONSUL_AWS_0XX[@]}"; do
       # shellcheck disable=SC2034
-      PRODUCT_NAME="consul-aws"
-      PRODUCT_VERSION="${VERSION}"
-      PRODUCT_VERSION_CLEAN="${PRODUCT_VERSION}"
-      PRODUCT_PACKAGE_PATH="${PRODUCT_NAME}_${PRODUCT_VERSION}_darwin_amd64.zip"
-      PRODUCT_CHECKSUM_URL="${PRODUCT_PACKAGE_HOST}/${PRODUCT_NAME}/${PRODUCT_VERSION}/${PRODUCT_NAME}_${PRODUCT_VERSION}_SHA256SUMS"
-      PRODUCT_CHECKSUM_PATTERN="${PRODUCT_PACKAGE_PATH}"
-
+      PACKAGE_PATH="${BINARY}_${VERSION}_darwin_amd64.zip"
+      CHECKSUM_URL="${CHECKSUM_URL_BASE}/${BINARY}/${VERSION}/${BINARY}_${VERSION}_SHA256SUMS"
+      CHECKSUM_PATTERN="${PACKAGE_PATH}"
       generate_cask
     done
     ;;
 
   consul-template)
-    # generates Casks for consul-template
+    BINARY="${1}"
+    NAME="${1}"
+    HOMEPAGE="https:\/\/github.com\/hashicorp\/consul-template"
+
     for VERSION in "${CONSUL_TEMPLATE_0XX[@]}"; do
       # shellcheck disable=SC2034
-      PRODUCT_NAME="consul-template"
-      PRODUCT_VERSION="${VERSION}"
-      PRODUCT_VERSION_CLEAN="${PRODUCT_VERSION}"
-      PRODUCT_PACKAGE_PATH="${PRODUCT_NAME}_${PRODUCT_VERSION}_darwin_amd64.zip"
-      PRODUCT_CHECKSUM_URL="${PRODUCT_PACKAGE_HOST}/${PRODUCT_NAME}/${PRODUCT_VERSION}/${PRODUCT_NAME}_${PRODUCT_VERSION}_SHA256SUMS"
-      PRODUCT_CHECKSUM_PATTERN="${PRODUCT_PACKAGE_PATH}"
-
+      PACKAGE_PATH="${BINARY}_${VERSION}_darwin_amd64.zip"
+      CHECKSUM_URL="${CHECKSUM_URL_BASE}/${BINARY}/${VERSION}/${BINARY}_${VERSION}_SHA256SUMS"
+      CHECKSUM_PATTERN="${PACKAGE_PATH}"
       generate_cask
     done
     ;;
 
   envconsul)
-    # generates Casks for envconsul
+    BINARY="${1}"
+    NAME="${1}"
+    HOMEPAGE="https:\/\/github.com\/hashicorp\/envconsul"
+
     for VERSION in "${ENVCONSUL_0XX[@]}"; do
       # shellcheck disable=SC2034
-      PRODUCT_NAME="envconsul"
-      PRODUCT_VERSION="${VERSION}"
-      PRODUCT_VERSION_CLEAN="${PRODUCT_VERSION}"
-      PRODUCT_PACKAGE_PATH="${PRODUCT_NAME}_${PRODUCT_VERSION}_darwin_amd64.zip"
-      PRODUCT_CHECKSUM_URL="${PRODUCT_PACKAGE_HOST}/${PRODUCT_NAME}/${PRODUCT_VERSION}/${PRODUCT_NAME}_${PRODUCT_VERSION}_SHA256SUMS"
-      PRODUCT_CHECKSUM_PATTERN="${PRODUCT_PACKAGE_PATH}"
-
+      PACKAGE_PATH="${BINARY}_${VERSION}_darwin_amd64.zip"
+      CHECKSUM_URL="${CHECKSUM_URL_BASE}/${BINARY}/${VERSION}/${BINARY}_${VERSION}_SHA256SUMS"
+      CHECKSUM_PATTERN="${PACKAGE_PATH}"
       generate_cask
     done
     ;;
 
   nomad)
-    # generates Casks for Nomad and sets prefix (`./`) for checksums
+    BINARY="${1}"
+    NAME="Nomad"
+    HOMEPAGE="https:\/\/www.nomadproject.io\/"
+
     for VERSION in "${NOMAD_PREFIXED[@]}"; do
-      generate_cask "nomad" "${VERSION}" "zip" "_darwin_amd64" "" "./"
+      # shellcheck disable=SC2034
+      PACKAGE_PATH="${BINARY}_${VERSION}_darwin_amd64.zip"
+      CHECKSUM_URL="${CHECKSUM_URL_BASE}/${BINARY}/${VERSION}/${BINARY}_${VERSION}_SHA256SUMS"
+      CHECKSUM_PATTERN="./${PACKAGE_PATH}"
+      generate_cask
     done
 
-    # generates Casks for Nomad
     for VERSION in "${NOMAD_NONPREFIXED[@]}"; do
-      generate_cask "nomad" "${VERSION}" "zip" "_darwin_amd64" ""
+      # shellcheck disable=SC2034
+      PACKAGE_PATH="${BINARY}_${VERSION}_darwin_amd64.zip"
+      CHECKSUM_URL="${CHECKSUM_URL_BASE}/${BINARY}/${VERSION}/${BINARY}_${VERSION}_SHA256SUMS"
+      CHECKSUM_PATTERN="${PACKAGE_PATH}"
+      generate_cask
     done
     ;;
 
   packer)
-    # generates Casks for Packer
+    BINARY="${1}"
+    NAME="Packer"
+    HOMEPAGE="https:\/\/www.packer.io\/"
+
     for VERSION in "${PACKER_1XX[@]}"; do
       # shellcheck disable=SC2034
-      PRODUCT_NAME="packer"
-      PRODUCT_VERSION="${VERSION}"
-      PRODUCT_VERSION_CLEAN="${PRODUCT_VERSION}"
-      PRODUCT_PACKAGE_PATH="${PRODUCT_NAME}_${PRODUCT_VERSION}_darwin_amd64.zip"
-      PRODUCT_CHECKSUM_URL="${PRODUCT_PACKAGE_HOST}/${PRODUCT_NAME}/${PRODUCT_VERSION}/${PRODUCT_NAME}_${PRODUCT_VERSION}_SHA256SUMS"
-      PRODUCT_CHECKSUM_PATTERN="${PRODUCT_PACKAGE_PATH}"
-
+      PACKAGE_PATH="${BINARY}_${VERSION}_darwin_amd64.zip"
+      CHECKSUM_URL="${CHECKSUM_URL_BASE}/${BINARY}/${VERSION}/${BINARY}_${VERSION}_SHA256SUMS"
+      CHECKSUM_PATTERN="${PACKAGE_PATH}"
       generate_cask
     done
     ;;
 
   sentinel)
-    # generates Casks for Sentinel
+    BINARY="sentinel"
+    NAME="Sentinel"
+    CASK_FILE="templates/sentinel.cask"
+
     for VERSION in "${SENTINEL_0XX[@]}"; do
-      generate_cask "sentinel" "${VERSION}" "zip" "_darwin_amd64" ""
+      # shellcheck disable=SC2034
+      PACKAGE_PATH="${BINARY}_${VERSION}_darwin_amd64.zip"
+      CHECKSUM_URL="${CHECKSUM_URL_BASE}/${BINARY}/${VERSION}/${BINARY}_${VERSION}_SHA256SUMS"
+      CHECKSUM_PATTERN="${PACKAGE_PATH}"
+      generate_cask
     done
     ;;
 
   vagrant)
-    # generates Casks for 32-bit versions of Vagrant
+    BINARY="${1}"
+    NAME="Vagrant"
+    HOMEPAGE="https:\/\/www.vagrantup.com\/"
+    CASK_FILE="templates/vagrant.cask"
+
     for VERSION in "${VAGRANT_32BIT[@]}"; do
       # Vagrant < 2.x.x requires untrusted packages to be trusted
       if [[ ${VERSION} < "2.0.1" ]]; then
-        generate_cask "vagrant" "${VERSION}" "dmg" "omit" ", allow_untrusted: true"
+        # shellcheck disable=SC2034
+        FLAGS=", allow_untrusted: true"
+        PACKAGE_PATH="${BINARY}_${VERSION}.dmg"
       else
-        generate_cask "vagrant" "${VERSION}" "dmg" "omit"
+        PACKAGE_PATH="${BINARY}_${VERSION}_x86_64.dmg"
       fi
+
+      CHECKSUM_PATTERN="${PACKAGE_PATH}"
+      CHECKSUM_URL="${CHECKSUM_URL_BASE}/${BINARY}/${VERSION}/${BINARY}_${VERSION}_SHA256SUMS"
+      generate_cask
     done
 
-    # generates Casks for 64-bit versions of Vagrant
     for VERSION in "${VAGRANT_64BIT[@]}"; do
+      # Vagrant < 2.x.x requires untrusted packages to be trusted
       if [[ ${VERSION} < "2.0.1" ]]; then
-        generate_cask "vagrant" "${VERSION}" "dmg" "_x86_64" ", allow_untrusted: true"
-      else
-        generate_cask "vagrant" "${VERSION}" "dmg" "_x86_64"
+        # shellcheck disable=SC2034
+        FLAGS=", allow_untrusted: true"
       fi
+
+      PACKAGE_PATH="${BINARY}_${VERSION}_x86_64.dmg"
+      CHECKSUM_PATTERN="${PACKAGE_PATH}"
+      CHECKSUM_URL="${CHECKSUM_URL_BASE}/${BINARY}/${VERSION}/${BINARY}_${VERSION}_SHA256SUMS"
+      generate_cask
     done
     ;;
 
   vault)
-    # generates Casks for Vault
+    BINARY="${1}"
+    NAME="Vault"
+    HOMEPAGE="https:\/\/www.vaultproject.io\/"
+
     for VERSION in "${VAULT_NONPREFIXED[@]}"; do
-      generate_cask "vault" "${VERSION}" "zip" "_darwin_amd64" ""
+      # shellcheck disable=SC2034
+      PACKAGE_PATH="${BINARY}_${VERSION}_darwin_amd64.zip"
+      CHECKSUM_URL="${CHECKSUM_URL_BASE}/${BINARY}/${VERSION}/${BINARY}_${VERSION}_SHA256SUMS"
+      CHECKSUM_PATTERN="${PACKAGE_PATH}"
+      generate_cask
     done
 
-    # generates Casks for Vault and sets prefix (`./`) for checksums
     for VERSION in "${VAULT_PREFIXED[@]}"; do
-      generate_cask "vault" "${VERSION}" "zip" "_darwin_amd64" "" "./"
+      # shellcheck disable=SC2034
+      PACKAGE_PATH="${BINARY}_${VERSION}_darwin_amd64.zip"
+      CHECKSUM_URL="${CHECKSUM_URL_BASE}/${BINARY}/${VERSION}/${BINARY}_${VERSION}_SHA256SUMS"
+      CHECKSUM_PATTERN="./${PACKAGE_PATH}"
+      generate_cask
     done
     ;;
 
